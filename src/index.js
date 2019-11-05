99a8a451c239b7bffddbb9c9d29a4e452fd8b6e8
flyhero <qfwang666@163.com>
2019-11-05 18:10:19 +0800

    feat:调整导出

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import iEditor from './lib/components/editor.vue';
 import iFullEditor from './lib/components/full-editor.vue';
-import Preview from './lib/components/preview.vue';
+import preview from './lib/components/preview.vue';
 import hljs from 'highlightjs/highlight.pack.js'
 Vue.use(hljs)
 const iviewEditor = {
-    iEditor,iFullEditor,Preview
+    iEditor
+};
+const iviewFullEditor = {
+    iFullEditor
+};
+const iPreview = {
+    preview
 };
 
 const installEditor = function(Vue, opts = {}) {
     Vue.component('i-editor', iEditor);
+};
+
+const installFullEditor = function(Vue, opts = {}) {
 	 Vue.component('i-full-editor', iFullEditor);
-	  Vue.component('i-preview', Preview);
+};
+
+const installPreview = function(Vue, opts = {}) {
+	  Vue.component('i-preview', preview);
 };
 
 if (typeof window !== 'undefined' && window.Vue) {
     installEditor(window.Vue);
+	installFullEditor(window.Vue);
+	installPreview(window.Vue);
 }
 
-export default Object.assign({ iviewEditor }, { installEditor});
+export default Object.assign({ iviewEditor,iviewFullEditor,iPreview }, { installEditor,installFullEditor,installPreview});
